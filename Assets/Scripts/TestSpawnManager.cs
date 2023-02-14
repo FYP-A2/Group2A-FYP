d0626dafc2a9bdf6506a07208d2e148efc890c3c
ycedwin <100751193+edwin-cyc@users.noreply.github.com>
2023-02-14 21:54:39 +0800

    effect

--- Assets/Scripts/TestSpawnManager.cs
+++ Assets/Scripts/TestSpawnManager.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class TestSpawnManager : MonoBehaviour
 {
-	public static TestSpawnManager Instance;
+    public static TestSpawnManager Instance;
 
-	Spawnpoint[] spawnpoints;
+    Spawnpoint[] spawnpoints;
 
-	public GameObject monsterPrefab;
-	void Awake()
-	{
-		Instance = this;
-		spawnpoints = GetComponentsInChildren<Spawnpoint>();
+    public GameObject monsterPrefab;
+    void Awake()
+    {
+        Instance = this;
+        spawnpoints = GetComponentsInChildren<Spawnpoint>();
         InvokeRepeating("SpawnEnemy", 0f, 5f);
     }
 
-	public Transform GetSpawnpoint()
-	{
-		return spawnpoints[Random.Range(0, spawnpoints.Length)].transform;
-	}
+    public Transform GetSpawnpoint()
+    {
+        return spawnpoints[Random.Range(0, spawnpoints.Length)].transform;
+    }
 
-	void SpawnEnemy()
-	{
-		Transform spawnpoint = GetSpawnpoint();
-		Instantiate(monsterPrefab, spawnpoint.position, spawnpoint.rotation);
+    public Transform GetSpawnpoint(Spawnpoint spawn)
+    {
+        return spawn.transform;
+    }
+
+    void SpawnEnemy()
+    {
+        //Transform spawnpoint = GetSpawnpoint();
+        //Instantiate(monsterPrefab, spawnpoint.position, spawnpoint.rotation);
+        Instantiate(monsterPrefab, GetSpawnpoint(spawnpoints[0]).position, GetSpawnpoint(spawnpoints[0]).rotation);
+        Instantiate(monsterPrefab, GetSpawnpoint(spawnpoints[1]).position, GetSpawnpoint(spawnpoints[1]).rotation);
+        Instantiate(monsterPrefab, GetSpawnpoint(spawnpoints[2]).position, GetSpawnpoint(spawnpoints[2]).rotation);
+        Instantiate(monsterPrefab, GetSpawnpoint(spawnpoints[3]).position, GetSpawnpoint(spawnpoints[3]).rotation);
     }
 }
