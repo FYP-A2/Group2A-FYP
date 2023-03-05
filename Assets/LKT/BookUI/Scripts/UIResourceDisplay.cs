4069b7a7af582528d2c3600c88030a75d3441997
LKT-E <100751615+LKT-E@users.noreply.github.com>
2023-03-05 18:18:22 +0800

    UI 0.2.1

--- Assets/LKT/BookUI/Scripts/UIResourceDisplay.cs
+++ Assets/LKT/BookUI/Scripts/UIResourceDisplay.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIResourceDisplay : MonoBehaviour
 {
     public ResourceGroupType playerResource;
+    public enum GetByWhat { NAME, INDEX}
+    public GetByWhat getByWhat = GetByWhat.NAME;
+
+    public Text textAmount;
+    public Image imageIcon;
+
+    string resourceName;
+    int resourceIndex = 0;
+
+    private void Update()
+    {
+        if (getByWhat == GetByWhat.NAME)
+        {
+            textAmount.text = playerResource.GetAmount(resourceName).ToString();
+            imageIcon.sprite = playerResource.FindResourceByName(resourceName).icon;
+        }
+        else
+        {
+            textAmount.text = playerResource.GetAmount(resourceIndex).ToString();
+            imageIcon.sprite = playerResource.resources[resourceIndex].icon;
+        }
+    }
 }
