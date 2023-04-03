7d2042873a4e12cb6ac858251c4d3446ba885e4f
unknown <a1@tyit35622.vtc.hk>
2023-04-03 14:11:14 +0800

    spawn call method

--- Assets/CYC/Scripts/NewSpawnManager.cs
+++ Assets/CYC/Scripts/NewSpawnManager.cs
@@ -26,7 +26,8 @@ public class NewSpawnManager : MonoBehaviour
         Instance = this;
         spawnPoints.AddRange(GetComponentsInChildren<Transform>());
         spawnPoints.Remove(transform);
-        StartCoroutine(SpawnPrefabs());
+        //StartCoroutine(SpawnPrefabs());
+        StartCoroutine(NewSpawnPrefabs(5));
     }
 
     private IEnumerator SpawnPrefabs()
@@ -77,7 +78,7 @@ public class NewSpawnManager : MonoBehaviour
                             float spawnDelay = Random.Range(spawnRate / 2, spawnRate);
                             yield return new WaitForSeconds(spawnDelay);
                         }
-                    }
+                    }    
                 }
             }
 
@@ -85,6 +86,7 @@ public class NewSpawnManager : MonoBehaviour
             {
                 yield return null;
             }
+            
         }
     }
 
@@ -104,5 +106,46 @@ public class NewSpawnManager : MonoBehaviour
         }
         result = Vector3.zero;
         return false;
+    }
+
+    private IEnumerator NewSpawnPrefabs(int level)
+    {
+            Debug.Log("Starting stage " + level);
+
+            foreach (EnemySpawnData data in enemySpawnData)
+            {
+                if (data.numToSpawn[level - 1] > 0)
+                {
+                    int numToSpawn = data.numToSpawn[level - 1];
+                    float totalSpawnTime = stageDuration;
+                    float spawnRate = totalSpawnTime / numToSpawn;
+
+                    int numSpawned = 0;
+                    while (numSpawned < numToSpawn)
+                    {
+                        int numToBurst = Random.Range(2, 6);
+                        float burstDelay = Random.Range(0.1f, 1.0f);
+                        yield return new WaitForSeconds(burstDelay);
+
+                        Vector3 spawnPoint = GetSpawnpoint().position;
+
+                        for (int i = 0; i < numToBurst && numSpawned < numToSpawn; i++)
+                        {
+                            if (RandomPoint(spawnPoint, range, out Vector3 point))
+                            {
+                                Instantiate(data.prefab, point, Quaternion.identity);
+                                numSpawned++;
+                            }
+                        }
+
+                        if (numSpawned < numToSpawn)
+                        {
+                            float spawnDelay = Random.Range(spawnRate / 2, spawnRate);
+                            yield return new WaitForSeconds(spawnDelay);
+                        }
+                    }
+                }
+            }
+            Debug.Log("End " + level);       
     }
 }
