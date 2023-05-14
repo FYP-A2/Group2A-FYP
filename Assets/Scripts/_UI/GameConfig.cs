1117b8e7ac220486bc5c126e2fd6e5b800c6d174
ycedwin <100751193+edwin-cyc@users.noreply.github.com>
2023-05-14 17:00:18 +0800

    bake navmesh

--- Assets/Scripts/_UI/GameConfig.cs
+++ Assets/Scripts/_UI/GameConfig.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameConfig : MonoBehaviour{
-    void Start(){ QualitySettings.vSyncCount = 1; Application.targetFrameRate = 60; }
+    void Start(){ QualitySettings.vSyncCount = 1; Application.targetFrameRate = 120; }
 }
