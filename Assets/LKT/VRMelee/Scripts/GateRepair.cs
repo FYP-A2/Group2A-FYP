e18c8da6a2ee73c62c5ff3d295e0c2c4e791f13b
LKT-E <100751615+LKT-E@users.noreply.github.com>
2023-06-11 23:52:29 +0800

    LKT temp

---
+++ Assets/LKT/VRMelee/Scripts/GateRepair.cs
@@ -0,0 +1,23 @@
+using FYP2A.VR.Melee.Target;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateRepair : MonoBehaviour
+{
+    MeleeTargetCommonBuild mtcb;
+    public GameObject gate;
+    public bool canBeRepair = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
